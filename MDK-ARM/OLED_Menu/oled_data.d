4ea10daae9b31a827eb6f817b8b1c57c371b31ab
Felixzuinb <340713446@qq.com>
2025-09-25 17:07:54 +0800

    update gitignore

--- MDK-ARM/OLED_Menu/oled_data.d
+++
@@ -1,3 +0,0 @@
-oled_menu\oled_data.o: ..\OLED\OLED_Data.c
-oled_menu\oled_data.o: ..\OLED\OLED_Data.h
-oled_menu\oled_data.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- MDK-ARM/OLED_Menu/oledfont.d
+++
@@ -1,2 +0,0 @@
-oled_menu\oledfont.o: ..\OLED\oledfont.c
-oled_menu\oledfont.o: ..\OLED\oledfont.h
